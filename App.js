efe6f5d1daa73e01d0f82fc47c8c13df05f57f19
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-22 18:25:58 -0300

    genero proyecto en expo y creo estructura

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';  
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+
+
+// Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+function myStack(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen />
+      <Stack.Screen />
+      <Stack.Screen />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
